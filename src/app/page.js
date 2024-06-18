7c203d35763b48e2507748897307844773f21d95
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-06-18 14:23:01 +0530

    adds navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import { ClickMe } from "./ClickMe";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <div>Hello World</div>
-      <ClickMe/>
+      <ClickMe/><br/>
+      <Link href="/home">Home</Link><br/>
+      <Link href="/cart">Cart</Link><br/>
+      <Link href="/home/nestedRoute">Nested Route</Link>
     </>
   ); 
 }
