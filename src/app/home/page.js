2d2d2ef987ce4d569c6a4d66248ce04e258da97f
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-06-18 14:53:30 +0530

    data fetching and server side rendering

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,7 +1,16 @@
 import React from "react"
 
-export default function HomePage(){
+export default async function HomePage(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await response.json()
     return(
-        <div>Home Page</div>
+        <>
+            <div>Home Page</div><br/>
+            <h3>USERS</h3><br/>
+            <ul>
+                {users.map(user => <li key={user.id}>{user.name}</li>)}
+            </ul>
+        </>
+        
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <div>Hello World</div>
-      <ClickMe/><br/>
+      <ClickMe/><br/><br/>
       <Link href="/home">Home</Link><br/>
       <Link href="/cart">Cart</Link><br/>
       <Link href="/home/nestedRoute">Nested Route</Link>
