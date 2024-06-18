b35caa4b3130fbdc3971aa6fbbe1cc2db22a3633
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-06-18 13:47:43 +0530

    adds client component

---
+++ src/app/ClickMe.js
@@ -0,0 +1,6 @@
+'use client'
+export function ClickMe(){
+    return (
+        <button onClick={() => console.log("clicked")}>Click me</button>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import { ClickMe } from "./ClickMe";
+
 export default function Home() {
   return (
     <>
       <div>Hello World</div>
+      <ClickMe/>
     </>
   ); 
 }
