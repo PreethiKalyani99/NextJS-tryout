dd53de081510e90b7456550d0e3599fe0a751f67
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-06-20 12:08:04 +0530

    creates pages directory

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,5 +1,16 @@
-export default function Cart(){
+export default async function Cart(){
+    const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users',
+        { next: { revalidate: 10 } }
+    )
+    const users = await response.json()
     return (
-        <div>Cart page</div>
+        <>
+            <div>Cart page</div>
+            <h3>USERS</h3><br/>
+            <ul>
+                {users.map(user => <li key={user.id}>{user.name}</li>)}
+            </ul>
+        </>
     )
 }
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,18 +1,9 @@
 import React from "react"
 
 export default async function HomePage(){
-    const response = await fetch(
-        'https://jsonplaceholder.typicode.com/users',
-        { next: { revalidate: 10 } }
-    )
-    const users = await response.json()
     return(
         <>
             <div>Home Page</div><br/>
-            <h3>USERS</h3><br/>
-            <ul>
-                {users.map(user => <li key={user.id}>{user.name}</li>)}
-            </ul>
         </>
         
     )
